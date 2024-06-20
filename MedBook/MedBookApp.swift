67c57ca121371d3522aca0581c035e14e0b22a52
appwithash <ashutoshpandey.developer@gmail.com>
2024-06-20 14:08:18 +0530

    Initial Commit

---
+++ MedBook/MedBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedBookApp.swift
+//  MedBook
+//
+//  Created by ashutosh on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MedBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
